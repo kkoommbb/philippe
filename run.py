dfe4300df23b73d7c54227dd040abbfb7d085239
Grigory Feldman <fgb631@yandex.ru>
2016-03-19 19:24:46 +0300

    Add settings and title

--- run.py
+++ run.py
@@ -2,6 +2,15 @@
 
 import markdown
 import codecs
+import json
+
+def get_settings():
+    settings_file = codecs.open('settings.json', 'r', encoding='utf-8')
+    settings = json.loads(settings_file.read())
+    settings_file.close()
+    return settings
+
+settings = get_settings()
 
 md = markdown.Markdown(extensions=['mdx_katex'])
 
@@ -10,6 +19,7 @@ header = u'''
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>{title}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css" type="text/css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.js"></script>
   </head>
@@ -23,7 +33,7 @@ mdfile = codecs.open('body.md', 'r', encoding='utf-8')
 mdcontent = mdfile.read()
 mdfile.close()
 
-html = header.format(body=md.convert(mdcontent))
+html = header.format(body=md.convert(mdcontent), title=settings['title'])
 
 outfile = codecs.open('index.html', 'w', 'utf-8')
 outfile.write(html)
